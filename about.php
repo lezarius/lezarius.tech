faf91090cd607db8ed5f0fef1985aff6fd015caf
Lezarius <remy.mazabraud@gmail.com>
2017-02-21 11:21:30 +0100

    add about page. (no content)

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+ <?php include 'menu.php'; ?>
+      
+ <div class="wrapper">
+   <div class="container">
+   <div class="row">
+   
+<p align="center">test</p>
+
+   </div>
+   </div>
+ </div>
+  
+ <?php include 'footer.php'; ?>
+    
+  </body>
+</html>
