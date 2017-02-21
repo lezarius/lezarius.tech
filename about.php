33be0b2309a9b2eca3058aa079e19225f6ab6e3f
Lezarius <remy.mazabraud@gmail.com>
2017-02-21 11:24:24 +0100

    test somethink

--- about.php
+++ about.php
@@ -2,15 +2,48 @@
 <html lang="en">
  <?php include 'menu.php'; ?>
       
- <div class="wrapper">
-   <div class="container">
-   <div class="row">
-   
-<p align="center">test</p>
-
-   </div>
-   </div>
- </div>
+        <div class="wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 wow fadeIn delay-05s">
+              <div class="banner-text">
+                <h2>Salut, je suis <span>Ronce</span> Mazabraud,</h2>
+                <p>Une scientifique d'internet <br>je fais de l'admin sys, et du dev web</p>
+              </div>
+              <div class="overlay-detail text-center">
+                  <a href="#about"><i class="fa fa-angle-down"></i></a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+  <section id="about" class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 text-right">
+          <h2 class="title-text">
+            Découvrire<br><span class="deco">Ronce</span> Mazabraud
+          </h2>
+        </div>
+        <div class="col-md-6 text-left">
+          <div class="about-text">
+            <p>Née avec la génération internet, je m'y suis mis très jeune. J'ai commencé comme beaucoup par jouée, avant de découvrire que je pouvais faire bien plus avec un simple ordinateur.</p>
+            <p>&nbsp;</p>
+            <p>Voila comment je suis née à nouveau y'a qu'elle que année j'ai découvert les joua de la programation web puis celle d'administrée des machine, puis un jours je suis tombé dans arch linux. Et me voici de tout coeur avec le libre et l'open source <3 </p>
+            <p>&nbsp;</p>
+            <ul class="abt-list">
+              <li>- Pro, logicielle libre.</li>
+              <li>- Linux love.</li>
+              <li>- RTFM best think ever.</li>
+              <li>- rm -rf / --force</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   
  <?php include 'footer.php'; ?>
     
