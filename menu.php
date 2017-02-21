2ba3f7b8bdc0589d54fd7ce24c76c66ed860ff3a
Lezarius <remy.mazabraud@gmail.com>
2017-02-21 10:59:42 +0100

    resize logo

--- menu.php
+++ menu.php
@@ -24,7 +24,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#"><img src="img/logo100.png"></a>
+              <a class="navbar-brand" href="#"><img src="img/logo100.png" height="50" width="50"></a>
             </div>
             <div class="collapse navbar-collapse" id="lauraMenu">
               <ul class="nav navbar-nav navbar-right navbar-border">
