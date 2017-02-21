425611b3e3c82a31f0fd4a6e494ca1eb209d10d6
Lezarius <remy.mazabraud@gmail.com>
2017-02-21 10:50:52 +0100

    Add menu and footer in another file

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="footer-2 text-center-xs bg--white">
+    <div class="container">
+      <!--end row-->
+      <div class="row">
+          <div class="col-md-6">
+              <div class="footer">
+                   MIT Lezarius.tech. <a href="https://github.com/lezarius/">github.com </a>
+                    <div class="credits">
+                        Designed with <a href="http://getbootstrap.com/">Bootstrap.</a>
+                    </div>
+              </div>
+          </div>
+          <div class="col-md-6 text-right">
+              <ul class="social-list">
+                  <li>
+                      <a href="twitter.com/lezarius">
+                          <i class="fa fa-twitter"></i>
+                      </a>
+                  </li>
+              </ul>
+          </div>
+          
+      </div>
+      <!--end row-->
+    </div>
+  </footer>
+  
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.bxslider.min.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="contactform/contactform.js"></script>
---
+++ menu.php
@@ -0,0 +1,40 @@
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lezarius.tech</title>
+    <meta name="description" content="Profesional website of lezarius">
+    <meta name="keywords" content="">
+    
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans|Roboto:300,400|Questrial|Satisfy">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  </head>
+  <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60" onload="myFunction()">
+  <div class="header">
+      <div class="bg-color">
+        <header id="main-header">
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#lauraMenu">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><img src="img/logo100.png">></a>
+            </div>
+            <div class="collapse navbar-collapse" id="lauraMenu">
+              <ul class="nav navbar-nav navbar-right navbar-border">
+                <li class="active"><a href="#main-header">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonial">Testimonial</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </header>
