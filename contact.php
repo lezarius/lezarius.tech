d856ab15d89201cbad430f7be4935a0026f39020
Lezarius <remy.mazabraud@gmail.com>
2017-03-20 18:33:21 +0100

    add contact info

---
+++ contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+ <?php include 'menu.php'; ?>
+      
+        <div class="wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 wow fadeIn delay-05s">
+              <div class="banner-text">
+                <h2>Page en construction</h2>
+                
+              </div>
+              <div class="overlay-detail text-center">
+                  <a href="#cv"><i class="fa fa-angle-down"></i></a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+  <section id="cv" class="section-padding wow fadeIn delay-05s">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 text-right">
+          <h2 class="title-text">
+            Contact<br><span class="deco">Ronce</span> Mazabraud
+          </h2>
+        </div>
+        <div class="col-md-6 text-left">
+          <div class="about-text">
+          <h3>Mail</h3>
+      <p>lezariusscience@gmail.com</p>
+          
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+  </section>
+  
+ <?php include 'footer.php'; ?>
+    
+  </body>
+</html>
