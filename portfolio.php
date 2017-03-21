645c8a2153116cb71f9d74939c873f25d363d6f3
Lezarius <remy.mazabraud@gmail.com>
2017-03-21 11:40:56 +0100

    add all website

--- portfolio.php
+++ portfolio.php
@@ -24,18 +24,23 @@
         <div class="col-md-6 text-left">
          <div class="about-text">
           <h3>Lezarius.tech</h3>
-      <p><a href="www.lezarius.tech">lezarius.tech</a> est mon site profesionel, disponible sous <a href="https://github.com/lezarius/lezarius.tech">github</a></p>
+      <p><a href="http://www.lezarius.tech/">lezarius.tech</a> est mon site profesionel, disponible sous <a href="https://github.com/lezarius/lezarius.tech">github</a></p>
       <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx + MariaDB, tout cela heberger sur mon serveur perso tournant sous debian 8.</p>
-          <h3>Téléphone :</h3>
-      <p>Mon téléphone, <a href="tel:+33753345477">07 53 34 54 77</a> , uniquement en appel. De 9h à 19h.</p>          
+        
+         <h3>CV and resume</h3>
+      <p><a href="http://www.cv.lezarius.tech/">cv.lezarius.tech</a> ou <a href="http://www.resume.lezarius.tech/">resume.lezarius.tech</a> sont mes cv en français et anglais, disponible sous github <a href="https://github.com/lezarius/CV">ici</a> ou <a href="https://github.com/lezarius/resume">ici</a> </p>
+      <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx, tout cela heberger sur mon serveur perso tournant sous debian 8.</p>        
           </div>
         </div>
         <div class="col-md-6 text-left">
           <div class="about-text">
-          <h3>Mail:</h3>
-      <p><a href="mailto:ronce@lezarius.tech">ronce@lezarius.tech</a></p>
-          <h3>Téléphone :</h3>
-      <p>Mon téléphone, <a href="tel:+33753345477">07 53 34 54 77</a> , uniquement en appel. De 9h à 19h.</p>          
+        <h3>Time to Science</h3>
+      <p><a href="http://blog.lezarius.science/">blog.lezarius.science</a> est un blog perso, je parle de tout et n'importe quoi dessus. Les article sont disponible sur <a href="https://github.com/lezarius/blogPost">github</a></p>
+      <p>Le blog utilise le CMS ghost, il est heberger sur mon dedié sous debian 8. Les techno utilisez sont node.js pour ghost, nginx pour liée le blog au nom de domaine, et mariaDB pour la base de donné.</p>
+
+      <h3>Lezarius.science</h3>
+      <p><a href="www.lezarius.science">lezarius.science</a> est mon site perso, disponible sous <a href="https://github.com/lezarius/Lezarius">github</a></p>
+      <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx + MariaDB, tout cela heberger sur mon serveur perso tournant sous debian 8.</p>         
           </div>
         </div>
 
