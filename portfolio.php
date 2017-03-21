2816e1807e1d75aeeef6fcdd43e7ce9b8e9d62f9
Lezarius <remy.mazabraud@gmail.com>
2017-03-21 11:04:44 +0100

    create portfolio page.

---
+++ portfolio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+ <?php include 'menu.php'; ?>
+      
+        <div class="wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 wow fadeIn delay-05s">
+              <div class="banner-text">
+                <h2>Page en cours de construction.</h2>
+                
+              </div>
+              <div class="overlay-detail text-center">
+                  <a href="#cv"><i class="fa fa-angle-down"></i></a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+  
+
+
+
+      </div>
+    </div>
+  </section>
+  
+ <?php include 'footer.php'; ?>
+    
+  </body>
+</html>
