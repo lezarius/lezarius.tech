b7cf22b57e6fd6dade66fff71fb0cc04bf5635dd
Lezarius <remy.mazabraud@gmail.com>
2017-03-21 17:13:29 +0100

    change some texte.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
           <div class="row">
             <div class="col-md-12 wow fadeIn delay-05s">
               <div class="banner-text">
-                <h2>Salut, je suis <span>Ronce</span> Mazabraud,</h2>
+                <h2>Bonjours, je suis <span>Ronce</span> Mazabraud,</h2>
                 <p>Une scientifique d'internet <br>je fais de l'administration système,<br> et du développement web</p>
               </div>
               <div class="overlay-detail text-center">
--- portfolio.php
+++ portfolio.php
@@ -21,26 +21,27 @@
       </div>
     <div class="container">
       <div class="row">
+      <h2>Tout mes service fonctionne sur mon propre dédié sous debian 8.</h2>
         <div class="col-md-6 text-left">
          <div class="about-text">
           <h3>Lezarius.tech</h3>
-      <p><a href="http://www.lezarius.tech/">lezarius.tech</a> est mon site profesionel, disponible sous <a href="https://github.com/lezarius/lezarius.tech">github</a></p>
-      <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx + MariaDB, tout cela heberger sur mon serveur perso tournant sous debian 8.</p>
+      <p><a href="http://www.lezarius.tech/">lezarius.tech</a> est mon site profesionel, <a href="https://github.com/lezarius/lezarius.tech">code source</a> disponible.</p>
+      <p>Les technologies utilisées pour la création du website sont le mélange audacieux de php-fpm(v7), Nginx.</p>
         
          <h3>CV and resume</h3>
-      <p><a href="http://www.cv.lezarius.tech/">cv.lezarius.tech</a> ou <a href="http://www.resume.lezarius.tech/">resume.lezarius.tech</a> sont mes cv en français et anglais, disponible sous github <a href="https://github.com/lezarius/CV">ici</a> ou <a href="https://github.com/lezarius/resume">ici</a> </p>
-      <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx, tout cela heberger sur mon serveur perso tournant sous debian 8.</p>        
+      <p><a href="http://www.cv.lezarius.tech/">cv.lezarius.tech</a> ou <a href="http://www.resume.lezarius.tech/">resume.lezarius.tech</a>  sont mes CV respectivement en français et en anglais. (Code source disponible <a href="https://github.com/lezarius/CV">ici</a> ou <a href="https://github.com/lezarius/resume">ici</a>) </p>
+      <p>Les techno utilisez pour l'hebergement sont php-fpm(v7) et Nginx.</p>        
           </div>
         </div>
         <div class="col-md-6 text-left">
           <div class="about-text">
         <h3>Time to Science</h3>
-      <p><a href="http://blog.lezarius.science/">blog.lezarius.science</a> est un blog perso, je parle de tout et n'importe quoi dessus. Les article sont disponible sur <a href="https://github.com/lezarius/blogPost">github</a></p>
-      <p>Le blog utilise le CMS ghost, il est heberger sur mon dedié sous debian 8. Les techno utilisez sont node.js pour ghost, nginx pour liée le blog au nom de domaine, et mariaDB pour la base de donné.</p>
+      <p><a href="http://blog.lezarius.science/">blog.lezarius.science</a> est un blog personel, je parle de tout et n'importe quoi dessus. Les article sont disponible sur <a href="https://github.com/lezarius/blogPost">github</a></p>
+      <p>Le blog utilise le CMS ghost. Les technologies utilisez sont node.js pour ghost, nginx pour liée le blog au nom de domaine, et mariaDB pour la base de donné.</p>
 
       <h3>Lezarius.science</h3>
-      <p><a href="www.lezarius.science">lezarius.science</a> est mon site perso, disponible sous <a href="https://github.com/lezarius/Lezarius">github</a></p>
-      <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx + MariaDB, tout cela heberger sur mon serveur perso tournant sous debian 8.</p>         
+      <p><a href="www.lezarius.science">lezarius.science</a> est mon site perso, <a href="https://github.com/lezarius/Lezarius">code source</a> disponible</p>
+      <p>Les techno utilisez pour l'hebergement sont le combo php-fpm(v7) + Nginx</p>         
           </div>
         </div>
 
