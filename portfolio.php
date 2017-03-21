6876ab6cd4c457705a40ea8061b782d2b77a183d
Lezarius <remy.mazabraud@gmail.com>
2017-03-21 11:09:34 +0100

    test design

--- portfolio.php
+++ portfolio.php
@@ -19,13 +19,29 @@
         </div>
         </div>
       </div>
-  
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 text-right">
+         <div class="about-text">
+          <h3>Mail:</h3>
+      <p><a href="mailto:ronce@lezarius.tech">ronce@lezarius.tech</a></p>
+          <h3>Téléphone :</h3>
+      <p>Mon téléphone, <a href="tel:+33753345477">07 53 34 54 77</a> , uniquement en appel. De 9h à 19h.</p>          
+          </div>
+        </div>
+        <div class="col-md-6 text-left">
+          <div class="about-text">
+          <h3>Mail:</h3>
+      <p><a href="mailto:ronce@lezarius.tech">ronce@lezarius.tech</a></p>
+          <h3>Téléphone :</h3>
+      <p>Mon téléphone, <a href="tel:+33753345477">07 53 34 54 77</a> , uniquement en appel. De 9h à 19h.</p>          
+          </div>
+        </div>
 
 
 
       </div>
     </div>
-  </section>
   
  <?php include 'footer.php'; ?>
     
